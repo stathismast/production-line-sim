12d8fbfa056c62267d4d4eff58aefa613b9b5221
stathismast <st.mastorakis@gmail.com>
2018-11-13 17:49:44 +0200

    Added checker processes

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <string.h>     // memcpy
 #include "psm.h"
 
-void painter(TriplePSM * input, int numOfItems){
+void painter(TriplePSM * input, TriplePSM * output, int numOfItems){
     Part part;
 
     for(int i=0; i<numOfItems; i++){
@@ -28,14 +28,19 @@ void painter(TriplePSM * input, int numOfItems){
                     memcpy(&part, input->psm[2]->sharedMemory,sizeof(Part));
                 semUp(input->psm[2]->semEmpty);
             }
-            
+
         semUp(input->semAllEmpty);
 
         printf("Received: %d/%d\n", part.type, part.id);
 
+        semDown(output->psm[part.type]->semEmpty);
+            memcpy(output->psm[part.type]->sharedMemory,&part,sizeof(Part));
+        semUp(output->psm[part.type]->semFull);
+
     }
     
     freeTriplePSM(input);
+    freeTriplePSM(output);
     exit(0);
 }
 
@@ -64,6 +69,17 @@ void construction(TriplePSM * output, int rank, int numOfItems){
     exit(0);
 }
 
+void checker(PSM * input, int numOfItems){
+    Part part;
+
+    for(int i=0; i<numOfItems; i++){
+        semDown(input->semFull);
+            memcpy(&part, input->sharedMemory,sizeof(Part));
+        semUp(input->semEmpty);
+        printf("Checking: %d/%d\n", part.type, part.id);
+    }
+}
+
 int main(int argc, char * argv[]){
     srand(time(NULL));
 
@@ -85,11 +101,37 @@ int main(int argc, char * argv[]){
     if(fork() == 0)
         construction(tpsm1, 2, numOfItems);
 
+    // Set up shared memory between the painter and the three checkers
+    TriplePSM * tpsm2 = getTriplePSM(sizeof(Part));
+
     // Create painter
     if(fork() == 0)
-        painter(tpsm1, 3*numOfItems);
+        painter(tpsm1, tpsm2, 3*numOfItems);
+
+    // Create three part producers
+    if(fork() == 0){
+        checker(tpsm2->psm[0], numOfItems);
+        freeTriplePSM(tpsm1);
+        freeTriplePSM(tpsm2);
+        exit(0);
+    }
+    if(fork() == 0){
+        checker(tpsm2->psm[1], numOfItems);
+        freeTriplePSM(tpsm1);
+        freeTriplePSM(tpsm2);
+        exit(0);
+    }
+    if(fork() == 0){
+        checker(tpsm2->psm[2], numOfItems);
+        freeTriplePSM(tpsm1);
+        freeTriplePSM(tpsm2);
+        exit(0);
+    }
 
     // Wait for every process to finish
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
     wait(NULL);
     wait(NULL);
     wait(NULL);
@@ -98,4 +140,7 @@ int main(int argc, char * argv[]){
     // Detach protected shared memory
     detachTriplePSM(tpsm1);
     freeTriplePSM(tpsm1);
+
+    detachTriplePSM(tpsm2);
+    freeTriplePSM(tpsm2);
 }
