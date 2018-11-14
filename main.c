c07e30c9c9f7446eadd2e44d4062e9830fc0718a
stathismast <st.mastorakis@gmail.com>
2018-11-14 15:30:50 +0200

    Added assembler. There is an issue with a bottleneck at the painter process, so I have included extra painter and construction processes that avoid this issue until we get a clarification

--- main.c
+++ main.c
@@ -4,6 +4,47 @@
 #include <string.h>     // memcpy
 #include "psm.h"
 
+void painterThatWorks(TriplePSM * input, TriplePSM * output, int numOfItems){
+    Part part;
+
+    for(int i=0; i<numOfItems/3; i++){
+        for(int j=0; j<3; j++){
+            semDown(input->psm[j]->semFull);
+                memcpy(&part, input->psm[j]->sharedMemory,sizeof(Part));
+            semUp(input->psm[j]->semEmpty);
+
+            printf("Received: %d/%d\n", part.type, part.id);
+            
+            semDown(output->psm[part.type]->semEmpty);
+                memcpy(output->psm[part.type]->sharedMemory,&part,sizeof(Part));
+            semUp(output->psm[part.type]->semFull);
+        }
+    }
+    
+    freeTriplePSM(input);
+    freeTriplePSM(output);
+    exit(0);
+}
+
+void constructionThatWorks(TriplePSM * output, int rank, int numOfItems){
+    srand(time(NULL)*getpid());
+    Part part;
+    part.type = rank;
+
+    for(int i=0; i<numOfItems; i++){
+        part.id = randomNumber(1000,10000); 
+        int contructionTime = randomNumber(5000,10000);
+        semDown(output->psm[rank]->semEmpty);
+            printf("Sending:  %d/%d\n", part.type, part.id);
+            usleep(contructionTime); // Sleep for half a second
+            memcpy(output->psm[rank]->sharedMemory,&part,sizeof(Part));
+        semUp(output->psm[rank]->semFull);
+    }
+
+    freeTriplePSM(output);
+    exit(0);
+}
+
 void painter(TriplePSM * input, TriplePSM * output, int numOfItems){
     Part part;
 
@@ -31,7 +72,7 @@ void painter(TriplePSM * input, TriplePSM * output, int numOfItems){
 
         semUp(input->semAllEmpty);
 
-        printf("Received: %d/%d\n", part.type, part.id);
+        // printf("Received: %d/%d\n", part.type, part.id);
 
         semDown(output->psm[part.type]->semEmpty);
             memcpy(output->psm[part.type]->sharedMemory,&part,sizeof(Part));
@@ -56,27 +97,74 @@ void construction(TriplePSM * output, int rank, int numOfItems){
         semDown(output->semAllEmpty);
 
             semDown(output->psm[rank]->semEmpty);
-                printf("Sending:  %d/%d\n", part.type, part.id);
+                // printf("Sending:  %d/%d\n", part.type, part.id);
                 usleep(contructionTime); // Sleep for half a second
                 memcpy(output->psm[rank]->sharedMemory,&part,sizeof(Part));
             semUp(output->psm[rank]->semFull);
 
         semUp(output->semAllFull);
-        part.id++;
     }
 
     freeTriplePSM(output);
     exit(0);
 }
 
-void checker(PSM * input, int numOfItems){
+void checker(PSM * input, TriplePSM * output, int numOfItems){
     Part part;
 
     for(int i=0; i<numOfItems; i++){
         semDown(input->semFull);
             memcpy(&part, input->sharedMemory,sizeof(Part));
         semUp(input->semEmpty);
+
         printf("Checking: %d/%d\n", part.type, part.id);
+
+        // semDown(output->semAllEmpty);
+
+            semDown(output->psm[part.type]->semEmpty);
+                printf("CSending: %d/%d\n", part.type, part.id);
+                // usleep(contructionTime); // Sleep for half a second
+                memcpy(output->psm[part.type]->sharedMemory,&part,sizeof(Part));
+            semUp(output->psm[part.type]->semFull);
+
+        semUp(output->semAllFull);
+    }
+}
+
+void assembler(TriplePSM * input, int numOfItems){
+    Part part[3];
+    int received[3] = {0};
+
+    for(int i=0; i<numOfItems; i++){
+
+        // if(i%100 == 0) printf("%d\n", i);
+
+        semDown(input->semAllFull); // If there is atleast one part ready
+
+            if(semValue(input->psm[0]->semFull) && !received[0]){
+                semDown(input->psm[0]->semFull);
+                    memcpy(&part[0], input->psm[0]->sharedMemory,sizeof(Part));
+                    received[0] = 1;
+            }
+            else if(semValue(input->psm[1]->semFull) && !received[1]){
+                semDown(input->psm[1]->semFull);
+                    memcpy(&part[1], input->psm[1]->sharedMemory,sizeof(Part));
+                    received[1] = 1;
+            }
+            else if(semValue(input->psm[2]->semFull) && !received[2]){
+                semDown(input->psm[2]->semFull);
+                    memcpy(&part[2], input->psm[2]->sharedMemory,sizeof(Part));
+                    received[2] = 1;
+            }
+
+        if(received[0] && received[1] && received[2]){
+            printf("Assembler: %d/%d & %d/%d & %d/%d\n", part[0].type, part[0].id, part[1].type, part[1].id, part[2].type, part[2].id);
+            received[0] = 0; received[1] = 0; received[2] = 0; 
+            semUp(input->psm[0]->semEmpty);
+            semUp(input->psm[1]->semEmpty);
+            semUp(input->psm[2]->semEmpty);
+            // semUp(input->semAllEmpty);
+        }
     }
 }
 
@@ -108,27 +196,42 @@ int main(int argc, char * argv[]){
     if(fork() == 0)
         painter(tpsm1, tpsm2, 3*numOfItems);
 
+    // Set up shared memory between the checkers and the assembler
+    TriplePSM * tpsm3 = getTriplePSM(sizeof(Part));
+
     // Create three part producers
     if(fork() == 0){
-        checker(tpsm2->psm[0], numOfItems);
+        checker(tpsm2->psm[0], tpsm3, numOfItems);
         freeTriplePSM(tpsm1);
         freeTriplePSM(tpsm2);
+        freeTriplePSM(tpsm3);
         exit(0);
     }
     if(fork() == 0){
-        checker(tpsm2->psm[1], numOfItems);
+        checker(tpsm2->psm[1], tpsm3, numOfItems);
         freeTriplePSM(tpsm1);
         freeTriplePSM(tpsm2);
+        freeTriplePSM(tpsm3);
         exit(0);
     }
     if(fork() == 0){
-        checker(tpsm2->psm[2], numOfItems);
+        checker(tpsm2->psm[2], tpsm3, numOfItems);
         freeTriplePSM(tpsm1);
         freeTriplePSM(tpsm2);
+        freeTriplePSM(tpsm3);
+        exit(0);
+    }
+
+    if(fork() == 0){
+        assembler(tpsm3, 3*numOfItems);
+        freeTriplePSM(tpsm1);
+        freeTriplePSM(tpsm2);
+        freeTriplePSM(tpsm3);
         exit(0);
     }
 
     // Wait for every process to finish
+    wait(NULL);
     wait(NULL);
     wait(NULL);
     wait(NULL);
